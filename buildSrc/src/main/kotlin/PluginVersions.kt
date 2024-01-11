dc19e21e05fd35535e2602288ec8d08ff10152f2
jiseok419 <pyosik0419@gmail.com>
2024-01-11 16:45:02 +0900

    set :: (#3) JPA Plugin 버전 설정

--- buildSrc/src/main/kotlin/PluginVersions.kt
+++ buildSrc/src/main/kotlin/PluginVersions.kt
@@ -2,6 +2,7 @@ object PluginVersions {
     const val SPRINGBOOT_VERSION = "3.0.4"
     const val DEPENDENCY_MANAGER_VERSION = "1.1.0"
     const val SPRING_PLUGIN_VERSION = "1.8.10"
+    const val JPA_PLUGIN_VERSION = "1.8.10"
     const val JVM_VERSION = "1.8.10"
     const val KAPT_VERSION = "1.7.10"
     const val ALLOPEN_VERSION = "1.6.21"
