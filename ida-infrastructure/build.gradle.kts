5d7f7b0080f87319c6f0cada7194ede4a26fab3b
jiseok419 <pyosik0419@gmail.com>
2024-01-11 16:44:52 +0900

    set :: (#3) JPA Plugin 설정

--- ida-infrastructure/build.gradle.kts
+++ ida-infrastructure/build.gradle.kts
@@ -1,12 +1,26 @@
 plugins {
+	kotlin("plugin.jpa") version PluginVersions.JPA_PLUGIN_VERSION
 	kotlin("plugin.spring") version PluginVersions.SPRING_PLUGIN_VERSION
 }
 
 dependencies {
+	implementation(project(":ida-domain"))
 	implementationDependencies(Libraries.Database)
 	implementationDependencies(Libraries.JPA)
 	implementationDependencies(Libraries.Querydsl)
 	implementationDependencies(Libraries.Test)
+}
+
+allOpen {
+	annotation("jakarta.persistence.Entity")
+	annotation("jakarta.persistence.MappedSuperclass")
+	annotation("jakarta.persistence.Embeddable")
+}
+
+noArg {
+	annotation("jakarta.persistence.Entity")
+	annotation("jakarta.persistence.MappedSuperclass")
+	annotation("jakarta.persistence.Embeddable")
 }
 
 tasks.getByName<Jar>("jar") {
