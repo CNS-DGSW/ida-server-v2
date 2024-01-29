164e2713ae58dfd8e23d9f221291454fd7bef2ae
jiseok419 <pyosik0419@gmail.com>
2024-01-30 08:54:08 +0900

    feat: Temporary config settings

---
+++ ida-http-application/src/main/kotlin/com/dgswiphak/ida/common/config/WebMvcConfig.kt
@@ -0,0 +1,17 @@
+package com.dgswiphak.ida.common.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedOrigins("*")
+            .allowCredentials(true)
+    }
+
+}
