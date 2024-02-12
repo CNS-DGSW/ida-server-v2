5f4b94f78afab70a1fc114821e45dd6da0f68b18
조완규 <101560095+pea06@users.noreply.github.com>
2024-02-12 23:42:48 +0900

    Feat :: (#15) 파일 업로드 로직 (#16)
    * feat: add file request dto
    
    * feat: add file save service
    
    * feat: add valid file extension util

---
+++ ida-domain/src/main/kotlin/com/dgswiphak/ida/common/dto/FileRequest.kt
@@ -0,0 +1,25 @@
+package com.dgswiphak.ida.common.dto
+
+data class FileRequest(
+    val filename: String,
+    val contentType: String,
+    val fileData: ByteArray
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as FileRequest
+
+        if (filename != other.filename) return false
+        if (!fileData.contentEquals(other.fileData)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = filename.hashCode()
+        result = 31 * result + fileData.contentHashCode()
+        return result
+    }
+}
