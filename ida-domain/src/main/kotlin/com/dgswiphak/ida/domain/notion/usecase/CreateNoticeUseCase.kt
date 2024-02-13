c21dc8551837db95d5f599e0030e2a66e4c5a48a
jiseok419 <pyosik0419@gmail.com>
2024-02-13 20:40:21 +0900

    feat :: (#17) Add Attached Download Feature

--- ida-domain/src/main/kotlin/com/dgswiphak/ida/domain/notion/usecase/CreateNoticeUseCase.kt
+++ ida-domain/src/main/kotlin/com/dgswiphak/ida/domain/notion/usecase/CreateNoticeUseCase.kt
@@ -1,23 +1,40 @@
 package com.dgswiphak.ida.domain.notion.usecase
 
 import com.dgswiphak.ida.common.annotation.UseCase
+import com.dgswiphak.ida.common.dto.FileRequest
+import com.dgswiphak.ida.common.file.FileService
 import com.dgswiphak.ida.domain.notion.dto.request.CreateNoticeRequest
 import com.dgswiphak.ida.domain.notion.model.Notice
+import com.dgswiphak.ida.domain.notion.model.value.Attached
 import com.dgswiphak.ida.domain.notion.spi.query.CommandNoticeSpi
 import java.time.LocalDateTime
 
 @UseCase
 class CreateNoticeUseCase(
-   private val commandNoticeSpi: CommandNoticeSpi
+    private val commandNoticeSpi: CommandNoticeSpi,
+    private val fileService: FileService
 ) {
 
-    fun execute(request: CreateNoticeRequest) {
+    fun execute(
+        noticeRequest: CreateNoticeRequest,
+        attachedRequest: List<FileRequest>
+    ) {
+        val attached = attachedRequest.map {
+            Attached(
+                originalName = it.filename,
+                filePath = fileService.save(
+                    "static/file",
+                    it
+                )
+            )
+        }
 
         val notice = Notice(
-            title = request.title,
-            content = request.content,
-            isMajor = request.isMajor,
+            title = noticeRequest.title,
+            content = noticeRequest.content,
+            isMajor = noticeRequest.isMajor,
             createdAt = LocalDateTime.now(),
+            attached = attached
         )
 
         commandNoticeSpi.save(notice)
--- ida-http-application/src/main/kotlin/com/dgswiphak/ida/aggregate/notice/NoticeWebAdapter.kt
+++ ida-http-application/src/main/kotlin/com/dgswiphak/ida/aggregate/notice/NoticeWebAdapter.kt
@@ -1,13 +1,22 @@
 package com.dgswiphak.ida.aggregate.notice
 
+import com.dgswiphak.ida.common.dto.FileRequest
 import com.dgswiphak.ida.domain.notion.dto.request.CreateNoticeRequest
 import com.dgswiphak.ida.domain.notion.dto.request.UpdateNoticeRequest
 import com.dgswiphak.ida.domain.notion.dto.response.NoticeResponse
 import com.dgswiphak.ida.domain.notion.dto.response.NoticesResponse
 import com.dgswiphak.ida.domain.notion.usecase.*
 import org.jetbrains.annotations.NotNull
+import org.springframework.core.io.Resource
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
+import java.io.UnsupportedEncodingException
+import java.lang.RuntimeException
+import java.net.URLEncoder
 
 @RestController
 @RequestMapping("/notice")
@@ -16,13 +25,24 @@ class NoticeWebAdapter(
     private val queryNoticeDetailsUseCase: QueryNoticeDetailsUseCase,
     private val queryNoticeAllUseCase: QueryNoticeAllUseCase,
     private val deleteNoticeUseCase: DeleteNoticeUseCase,
-    private val updateNoticeUseCase: UpdateNoticeUseCase
+    private val updateNoticeUseCase: UpdateNoticeUseCase,
+    private val attachedDownloadUseCase: AttachedDownloadUseCase
 ) {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun createNotice(@RequestBody request: CreateNoticeRequest) {
-        createNoticeUseCase.execute(request)
+    fun createNotice(
+        @RequestPart("date") noticeRequest: CreateNoticeRequest,
+        @RequestPart("file") multipartFile: List<MultipartFile>
+    ) {
+        val fileRequest = multipartFile.map {
+            FileRequest(
+                filename = it.originalFilename!!,
+                contentType = it.contentType!!,
+                fileData = it.bytes
+            )
+        }
+        createNoticeUseCase.execute(noticeRequest, fileRequest)
     }
 
     @DeleteMapping("/{notice-id}")
@@ -47,5 +67,23 @@ class NoticeWebAdapter(
     @GetMapping
     fun queryAllNotices(): NoticesResponse {
         return queryNoticeAllUseCase.execute()
+    }
+
+    @GetMapping("/file/{notice-id}/{fileName}")
+    fun downloadAttached(
+        @PathVariable("notice-id") @NotNull noticeId: Long,
+        @PathVariable("fileName") @NotNull fileName: String
+    ): ResponseEntity<Resource> {
+        val resource = attachedDownloadUseCase.execute(noticeId, fileName)
+        try {
+            val originalName = URLEncoder.encode(fileName, "UTF-8")
+            return ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=$originalName")
+                    .body(resource)
+
+        } catch (e: UnsupportedEncodingException) {
+            throw RuntimeException()
+        }
     }
 }
