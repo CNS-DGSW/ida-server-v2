e9b032225191eca59e54df53c32d9e8f70119684
jiseok419 <pyosik0419@gmail.com>
2024-02-13 20:32:00 +0900

    feat :: (#17) Add file download functionality

--- ida-infrastructure/src/main/kotlin/com/dgswiphak/ida/common/service/FileServiceImpl.kt
+++ ida-infrastructure/src/main/kotlin/com/dgswiphak/ida/common/service/FileServiceImpl.kt
@@ -3,10 +3,15 @@ package com.dgswiphak.ida.common.service
 import com.dgswiphak.ida.common.dto.FileRequest
 import com.dgswiphak.ida.common.file.FileService
 import com.dgswiphak.ida.common.util.IdGenerator
+import org.springframework.core.io.Resource
+import org.springframework.core.io.UrlResource
 import org.springframework.stereotype.Service
 import java.io.File
+import java.net.MalformedURLException
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
+
 
 @Service
 class FileServiceImpl: FileService {
@@ -25,6 +30,19 @@ class FileServiceImpl: FileService {
             return filePath.toString()
         } catch (e: Exception) {
             throw RuntimeException("Failed to save file", e)
+        }
+    }
+
+    override fun download(path: String): Resource {
+        val filePath: Path = Paths.get(path)
+        try {
+            val resource: Resource = UrlResource(filePath.toUri())
+            if (!resource.exists() || !resource.isFile) {
+                throw RuntimeException("file not found : $filePath")
+            }
+            return resource
+        } catch (e: MalformedURLException) {
+            throw RuntimeException()
         }
     }
 
