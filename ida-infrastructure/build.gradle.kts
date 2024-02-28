e5d078fb3e6adf9d744452b3edc9316f1a28d192
pea06 <jowangyu75@dgsw.hs.kr>
2024-02-29 00:21:05 +0900

    chore: add feign dependency

--- ida-infrastructure/build.gradle.kts
+++ ida-infrastructure/build.gradle.kts
@@ -1,10 +1,19 @@
 plugins {
 	kotlin("plugin.jpa") version PluginVersions.JPA_PLUGIN_VERSION
 	kotlin("plugin.spring") version PluginVersions.SPRING_PLUGIN_VERSION
+	id("io.spring.dependency-management") version PluginVersions.DEPENDENCY_MANAGER_VERSION
 }
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:2022.0.5")
+	}
+}
+
 
 dependencies {
 	implementation(project(":ida-domain"))
+	implementationDependencies(Libraries.Feign)
 	implementationDependencies(Libraries.Database)
 	implementationDependencies(Libraries.Redis)
 	implementationDependencies(Libraries.JWT)
