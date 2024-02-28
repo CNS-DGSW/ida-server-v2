db51bb919e341f730cbb81918bff0d136c2cbdc1
jiseok419 <pyosik0419@gmail.com>
2024-02-28 20:14:31 +0900

    hotfix :: Security cors apply

--- ida-http-application/src/main/kotlin/com/dgswiphak/ida/global/config/WebSecurityConfiguration.kt
+++ ida-http-application/src/main/kotlin/com/dgswiphak/ida/global/config/WebSecurityConfiguration.kt
@@ -3,6 +3,7 @@ package com.dgswiphak.ida.global.config
 import com.dgswiphak.ida.global.filter.JwtFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -17,21 +18,20 @@ class WebSecurityConfiguration(
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http.csrf().disable()
-                .cors().disable()
-                .formLogin().disable()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeHttpRequests()
-                .requestMatchers("/auth/**").permitAll()
-                .requestMatchers("/member/signUp").permitAll()
-                .requestMatchers("/notice/**").permitAll()
-                .requestMatchers("/question/**").permitAll()
-                .requestMatchers("/attached/**").permitAll()
-                .requestMatchers("/image/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .cors(Customizer.withDefaults())
+            .formLogin().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+        http.authorizeHttpRequests()
+            .requestMatchers("/auth/**").permitAll()
+            .requestMatchers("/member/signUp").permitAll()
+            .requestMatchers("/notice/**").permitAll()
+            .requestMatchers("/question/**").permitAll()
+            .requestMatchers("/attached/**").permitAll()
+            .requestMatchers("/image/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
         return http.build()
     }
 }
