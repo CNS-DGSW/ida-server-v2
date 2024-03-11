57478ea502a5646278a354bb103a494b45fea0a7
jiseok419 <pyosik0419@gmail.com>
2024-03-11 14:12:40 +0900

    feat :: Modifying the Spring Security Policy

--- ida-http-application/src/main/kotlin/com/dgswiphak/ida/global/config/WebSecurityConfiguration.kt
+++ ida-http-application/src/main/kotlin/com/dgswiphak/ida/global/config/WebSecurityConfiguration.kt
@@ -4,6 +4,7 @@ import com.dgswiphak.ida.domain.member.model.value.Role
 import com.dgswiphak.ida.global.filter.JwtFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -25,6 +26,16 @@ class WebSecurityConfiguration(
 
         http.authorizeHttpRequests()
             .requestMatchers("/applicant/**").hasRole(Role.ROLE_APPLICANT.role)
+
+            .requestMatchers(HttpMethod.POST,"/notice").hasRole(Role.ROLE_TEACHER.role)
+            .requestMatchers(HttpMethod.PATCH,"/notice/{notice-id}").hasRole(Role.ROLE_TEACHER.role)
+            .requestMatchers(HttpMethod.DELETE,"/notice/{notice-id}").hasRole(Role.ROLE_TEACHER.role)
+
+            .requestMatchers(HttpMethod.PUT,"/image/save").hasRole(Role.ROLE_TEACHER.role)
+            .requestMatchers(HttpMethod.DELETE,"/image/{image-name}").hasRole(Role.ROLE_TEACHER.role)
+
+            .requestMatchers(HttpMethod.PUT,"/attached/{notice-id}").hasRole(Role.ROLE_TEACHER.role)
+            .requestMatchers(HttpMethod.DELETE,"/attached/{notice-id}/{file-name}").hasRole(Role.ROLE_TEACHER.role)
             .anyRequest().permitAll()
             .and()
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
