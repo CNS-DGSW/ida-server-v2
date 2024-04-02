de26e9f129ec09e7877a00850bf218eb1a57ba9b
pea06 <jowangyu75@dgsw.hs.kr>
2024-04-02 20:44:50 +0900

    refactor: BCryptPasswordEncoder to lsh encoder migration

--- ida-domain/src/main/kotlin/com/dgswiphak/ida/common/util/PasswordUtil.kt
+++ ida-domain/src/main/kotlin/com/dgswiphak/ida/common/util/PasswordUtil.kt
@@ -1,7 +1,7 @@
 package com.dgswiphak.ida.common.util
 
 interface PasswordUtil {
-    fun encoded(raw: String): String
+    fun encoded(raw: CharSequence): String
 
-    fun matches(raw: String, encoded: String): Boolean
+    fun matches(raw: CharSequence, encoded: String): Boolean
 }
