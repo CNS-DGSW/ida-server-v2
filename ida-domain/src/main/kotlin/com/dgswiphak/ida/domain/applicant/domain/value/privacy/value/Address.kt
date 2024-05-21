59d2b0dfad1c458725509119657b9a96c0bea5af
pea06 <jowangyu75@dgsw.hs.kr>
2024-05-21 19:34:38 +0900

    refactor: add address vo validation

--- ida-domain/src/main/kotlin/com/dgswiphak/ida/domain/applicant/domain/value/privacy/value/Address.kt
+++
@@ -1,7 +0,0 @@
-package com.dgswiphak.ida.domain.applicant.domain.value.privacy.value
-
-data class Address(
-    val detailAddress: String,
-    val streetAddress: String,
-    val zipCode: Int? = -1
-)
