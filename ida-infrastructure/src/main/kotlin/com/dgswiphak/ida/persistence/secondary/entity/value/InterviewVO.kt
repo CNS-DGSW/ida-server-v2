7bcf6908122aeef17cf624172bee2d9c71eeaec5
pea06 <jowangyu75@dgsw.hs.kr>
2024-06-03 11:39:16 +0900

    refactor: remove Serializable

--- ida-infrastructure/src/main/kotlin/com/dgswiphak/ida/persistence/secondary/entity/value/InterviewVO.kt
+++ ida-infrastructure/src/main/kotlin/com/dgswiphak/ida/persistence/secondary/entity/value/InterviewVO.kt
@@ -1,8 +1,6 @@
 package com.dgswiphak.ida.persistence.secondary.entity.value
 
 import jakarta.persistence.Embeddable
-import java.io.Serial
-import java.io.Serializable
 
 @Embeddable
 class InterviewVO(
@@ -11,11 +9,4 @@ class InterviewVO(
 
     val computingCompetence: Int? = 0
 
-) : Serializable {
-
-    companion object {
-        @Serial
-        private const val serialVersionUID: Long = -437300054465939296L
-    }
-
-}
+)
