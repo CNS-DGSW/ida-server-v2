dee4338670f1db543c27f86c1221b032409f503c
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-06-14 15:05:48 +0900

    chore :: 테스트 라이브러리를 번들로 정리 및 필요없는 의존성 제거
    테스트 라이브러리를 비롯한 기타 의존성이 libs.bundles.test 에서 관리되도록 정리했습니다.
    또한 기존에 사용되던 필요없는 의존성을 제거하였습니다.

--- ida-domain/build.gradle.kts
+++ ida-domain/build.gradle.kts
@@ -1,10 +1,3 @@
-plugins {
-    alias(libs.plugins.allopen)
-}
-
-allOpen { annotation("com.dgswiphak.ida.common.annotation.UseCase") }
-
 dependencies {
-    testImplementation(libs.kotest)
-    testImplementation(libs.mockk)
+    testImplementation(libs.bundles.test)
 }
