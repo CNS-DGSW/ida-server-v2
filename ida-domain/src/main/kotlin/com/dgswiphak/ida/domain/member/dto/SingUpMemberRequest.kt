3af8210acffab00837e2dda1af5c2fa1aa2162ad
Sewook <sewook0303@dgsw.hs.kr>
2024-06-18 09:21:25 +0900

    feat: 이메일 형식 검사 추가

--- ida-domain/src/main/kotlin/com/dgswiphak/ida/domain/member/dto/SingUpMemberRequest.kt
+++ ida-domain/src/main/kotlin/com/dgswiphak/ida/domain/member/dto/SingUpMemberRequest.kt
@@ -1,8 +1,10 @@
 package com.dgswiphak.ida.domain.member.dto
 
+import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotEmpty
 
 data class SingUpMemberRequest(
+        @field:Email(message = "Must be a valid email address")
         @field:NotEmpty(message = "email is required")
         val email: String,
         @field:NotEmpty(message = "password is required")
