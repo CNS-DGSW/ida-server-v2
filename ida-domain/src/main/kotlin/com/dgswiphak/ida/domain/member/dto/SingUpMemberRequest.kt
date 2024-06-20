1d30eb555bba6125131f755d69d1a88e73ff1c67
Sewook <sewook0303@dgsw.hs.kr>
2024-06-20 16:39:40 +0900

    feat: 비밀번호 유효성 검사 추가

--- ida-domain/src/main/kotlin/com/dgswiphak/ida/domain/member/dto/SingUpMemberRequest.kt
+++ ida-domain/src/main/kotlin/com/dgswiphak/ida/domain/member/dto/SingUpMemberRequest.kt
@@ -1,5 +1,6 @@
 package com.dgswiphak.ida.domain.member.dto
 
+import com.dgswiphak.ida.common.annotation.ValidPassword
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotEmpty
 
@@ -8,5 +9,6 @@ data class SingUpMemberRequest(
         @field:NotEmpty(message = "email is required")
         val email: String,
         @field:NotEmpty(message = "password is required")
+        @field:ValidPassword
         val password: String
 )
--- ida-domain/src/main/kotlin/com/dgswiphak/ida/domain/question/dto/request/UpdateQuestionRequest.kt
+++ ida-domain/src/main/kotlin/com/dgswiphak/ida/domain/question/dto/request/UpdateQuestionRequest.kt
@@ -1,10 +1,10 @@
 package com.dgswiphak.ida.domain.question.dto.request
 
-import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
 
 data class UpdateQuestionRequest(
-    @field:NotEmpty(message = "title is required")
+    @field:NotNull(message = "title is required")
     val title: String,
-    @field:NotEmpty(message = "content is required")
+    @field:NotNull(message = "content is required")
     val content: String
 )
