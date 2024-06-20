61c034743f4e4a822e6521a79dd4466dfc686a8f
Sewook <sewook0303@dgsw.hs.kr>
2024-06-21 08:33:46 +0900

    refactor: 비밀번호 유효성 검사 시 자체 valid annotaion 삭제 후 정규식으로 변경

--- ida-domain/src/main/kotlin/com/dgswiphak/ida/domain/member/dto/SingUpMemberRequest.kt
+++ ida-domain/src/main/kotlin/com/dgswiphak/ida/domain/member/dto/SingUpMemberRequest.kt
@@ -1,14 +1,14 @@
 package com.dgswiphak.ida.domain.member.dto
 
-import com.dgswiphak.ida.common.annotation.ValidPassword
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.Pattern
 
 data class SingUpMemberRequest(
         @field:Email(message = "Must be a valid email address")
         @field:NotEmpty(message = "email is required")
         val email: String,
         @field:NotEmpty(message = "password is required")
-        @field:ValidPassword
+        @field:Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[~!@#\\\$%\\^&\\*\\|\\''\":₩\\\\?]).{9,}", message = "Invalid Password")
         val password: String
 )
