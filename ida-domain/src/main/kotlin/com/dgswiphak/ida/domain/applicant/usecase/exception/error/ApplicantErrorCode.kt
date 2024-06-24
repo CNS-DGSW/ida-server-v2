8fd9b0d77f66a398a0d83092e3376e76dbaf40b0
최미래 <miraechoi@dgsw.hs.kr>
2024-06-24 20:16:31 +0900

    remove :: remove TYPE_ERROR in ApplicantErrorCode #63

--- ida-domain/src/main/kotlin/com/dgswiphak/ida/domain/applicant/usecase/exception/error/ApplicantErrorCode.kt
+++ ida-domain/src/main/kotlin/com/dgswiphak/ida/domain/applicant/usecase/exception/error/ApplicantErrorCode.kt
@@ -7,8 +7,7 @@ enum class ApplicantErrorCode(
     private val status: Int,
     private val message: String
 ) : ErrorProperty {
-    NOT_FOUND(ErrorStatus.NOT_FOUND, "해당 자료가 존재하지 않습니다."),
-    TYPE_ERROR(ErrorStatus.BAD_REQUEST, "파일 형식이 잘못 되었습니다.");
+    NOT_FOUND(ErrorStatus.NOT_FOUND, "해당 자료가 존재하지 않습니다.");
 
     override fun status(): Int = status
     override fun message(): String = message
