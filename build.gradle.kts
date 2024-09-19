6371e2e86fd3698a11cdd0d81e8b94180668056f
pea06 <jowangyu75@dgsw.hs.kr>
2024-09-19 20:25:12 +0900

    refactor: kotest-extensions-spring 삭제

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,6 +70,5 @@ allprojects {
         testImplementation("org.jetbrains.kotlin:kotlin-test")
         testImplementation("io.kotest:kotest-runner-junit5:5.3.2")
         testImplementation("io.kotest:kotest-assertions-core:5.3.2")
-        testImplementation("io.kotest.extensions:kotest-extensions-spring:5.8.1")
     }
 }
