ee608a348a6e9413920465a4c0aebc29ff723e6b
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-09-20 15:21:25 +0900

    feat: 진행 상태 조회 API 추가
    진행 상태를 조회할 수 있는 API를 새로 추가했습니다.

---
+++ admission/admission-application/src/main/kotlin/com/daegusw/apply/admission/application/service/ProgressService.kt
@@ -0,0 +1,24 @@
+package com.daegusw.apply.admission.application.service
+
+import com.daegusw.apply.admission.application.common.exception.AdmissionDoesNotExistException
+import com.daegusw.apply.admission.application.port.`in`.web.ProgressUseCase
+import com.daegusw.apply.admission.application.port.out.persistence.QueryAdmissionPort
+import com.daegusw.apply.admission.domain.admission.constant.Progress
+import com.daegusw.apply.member.id.MemberId
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class ProgressService (
+    private val queryAdmissionPort: QueryAdmissionPort
+) : ProgressUseCase{
+
+    @Transactional(readOnly = true)
+    override fun find(id: MemberId): Progress {
+        val admission = queryAdmissionPort.findByMemberId(id) ?: throw AdmissionDoesNotExistException(id)
+
+        return admission.progress!!
+
+    }
+
+}
---
+++ admission/admission-web-adapter/src/main/kotlin/com/daegusw/apply/admission/web/adapter/ProgressController.kt
@@ -0,0 +1,22 @@
+package com.daegusw.apply.admission.web.adapter
+
+import com.daegusw.apply.admission.application.port.`in`.web.ProgressUseCase
+import com.daegusw.apply.admission.web.adapter.response.ProgressResponse
+import com.daegusw.apply.core.authentication.AuthenticatedPrincipalId
+import com.daegusw.apply.member.id.MemberId
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/admission/progress")
+class ProgressController (
+    private val progressUseCase: ProgressUseCase
+){
+    @GetMapping
+    fun findProgress(
+        @AuthenticatedPrincipalId id: MemberId
+    ): ProgressResponse {
+        return ProgressResponse(progressUseCase.find(id))
+    }
+}
