d4f839cb5686b56bdffc68f38a290f3759f83ce7
pea06 <jowangyu75@dgsw.hs.kr>
2024-09-23 11:20:32 +0900

    refactor: dead auth-security-adapter module

--- auth/auth-security-adapter/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-dependencies {
-    implementation(project(":auth:auth-application"))
-
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-}
