0dfadde12ef67a1cf60e62944e4defb0f8530ee7
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-09-24 10:49:57 +0900

    Fix :: 오류 메시지 수정
    1차 전형과 최종 전형의 날짜 및 결과 메시지를 올바르게 업데이트했습니다. 올바른 날짜와 합격/탈락 결과가 표시되도록 수정했습니다.

--- admission/admission-domain/src/main/kotlin/com/daegusw/apply/admission/domain/admission/constant/LastAdmissionStatus.kt
+++ admission/admission-domain/src/main/kotlin/com/daegusw/apply/admission/domain/admission/constant/LastAdmissionStatus.kt
@@ -3,7 +3,7 @@ package com.daegusw.apply.admission.domain.admission.constant
 enum class LastAdmissionStatus(
     val message: String
 ) {
-    NONE("1차 전형 발표일은 2024년10월23일 수요일 오전 10:00시 입니다."),
-    LAST_ROUND_PASS("1차 전형 합격입니다."),
-    LAST_ROUND_FAIL("1차 전형 탈락입니다.")
+    NONE("최종 결과 발표일은 2024년10월30일 수요일 오전 10:00시 입니다."),
+    LAST_ROUND_PASS("최종 합격입니다."),
+    LAST_ROUND_FAIL("최종 탈락입니다.")
 }
