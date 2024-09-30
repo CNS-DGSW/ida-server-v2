2096aba04fad69ffb251da28430e426c9e1fb02a
pea06 <jowangyu75@dgsw.hs.kr>
2024-09-30 09:42:00 +0900

    feat: add excel file download response extension

--- core/file/src/main/kotlin/com/daegusw/apply/core/file/FileExtension.kt
+++ core/file/src/main/kotlin/com/daegusw/apply/core/file/FileExtension.kt
@@ -1,9 +1,12 @@
 package com.daegusw.apply.core.file
 
+import org.springframework.http.HttpHeaders
 import org.springframework.web.multipart.MultipartFile
 import java.io.File
 import java.io.FileOutputStream
+import java.net.URLEncoder
 import java.util.*
+import javax.servlet.http.HttpServletResponse
 
 fun MultipartFile.toFile(): File {
     val fileName = "${UUID.randomUUID()}_${this.originalFilename}"
@@ -14,4 +17,13 @@ fun MultipartFile.toFile(): File {
     }
 
     return file
+}
+
+fun HttpServletResponse.setExcelContentDisposition(fileName: String) {
+    setHeader(
+        HttpHeaders.CONTENT_DISPOSITION,
+        "attachment; filename=${URLEncoder.encode(fileName, "UTF-8")}.xlsx"
+    )
+
+    contentType = "ms-vnd/excel"
 }
